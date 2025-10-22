24edbeb74fed863bdde7b7c258d05866e49dd5df
Your Name <you@example.com>
2025-10-22 10:42:29 +0800

    remove console out put

--- core/server.go
+++ core/server.go
@@ -31,12 +31,5 @@ func RunServer() {
 
 	Router := initialize.Routers()
 	address := fmt.Sprintf(":%d", global.GVA_CONFIG.System.Addr)
-	fmt.Printf(`
-	项目地址：https://github.com/flipped-aurora/gin-vue-admin
-	插件市场:https://plugin.gin-vue-admin.com
-	GVA讨论社区:https://support.qq.com/products/371961
-	默认自动化文档地址:http://127.0.0.1%s/swagger/index.html
-	默认前端文件运行地址:http://127.0.0.1:8080
-`, address)
 	initServer(address, Router, 10*time.Minute, 10*time.Minute)
 }
