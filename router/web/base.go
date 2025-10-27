3460a35461de790c6b58521b63596a881bed8671
Your Name <you@example.com>
2025-10-27 08:46:43 +0800

    update limit

--- router/web/base.go
+++ router/web/base.go
@@ -9,8 +9,9 @@ type BaseRouter struct {
 }
 
 func (r BaseRouter) InitBaseRouter(Router *gin.RouterGroup) {
-	Router.GET("customer-service/config", baseApi.CustomerServiceConfig)       // 获取站点客服配置
-	Router.GET("captcha", middleware.CaptchaLimit(), baseApi.Captcha)          // 获取验证码
-	Router.POST("base/register", middleware.RegisterLimit(), baseApi.Register) // 用户注册
-	Router.POST("base/login", middleware.LoginLimit(), baseApi.Login)          // 用户登录
+	Router.GET("customer-service/config", baseApi.CustomerServiceConfig) // 获取站点客服配置
+	Router.GET("captcha", middleware.CaptchaLimit(), baseApi.Captcha)    // 获取验证码
+	//Router.POST("base/register", middleware.RegisterLimit(), baseApi.Register) // 用户注册
+	Router.POST("base/register", baseApi.Register) // 用户注册
+	Router.POST("base/login", baseApi.Login)       // 用户登录
 }
